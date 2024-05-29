31ce34ffb17ce0851f9f53b29cc62a99c25d662b
Sebastian Kuckluck <ghoma@outlook.de>
2024-05-29 10:52:21 +0200

    Added UML Classes

---
+++ ToDoApp/Klassen/ToDoItem.cs
@@ -0,0 +1,31 @@
+﻿namespace ToDoApp.Klassen
+{
+	public class ToDoItem
+	{
+		private int m_itemID;
+		private string m_itemTitle;
+		private string m_itemDescription;
+		private DateTime m_creationDate;
+		private DateTime m_dueDate;
+		private ItemState m_itemState;
+		private string m_itemCategory;
+		private string m_itemOwner;
+
+		public ToDoItem()
+		{
+
+		}
+
+		public void MarkAsCompleted()
+		{
+
+		}
+	}
+
+	enum ItemState
+	{
+		Open = 0,
+		inBearbeitung = 1,
+		Closed = 2
+	}
+}
---
+++ ToDoApp/Klassen/ToDoList.cs
@@ -0,0 +1,32 @@
+﻿using ToDoApp.Klassen;
+
+namespace ToDoApp.Klassen
+{
+	public class ToDoList
+	{
+		private int m_listID;
+		private string m_listName;
+
+		public ToDoList(int listID, string listName)
+		{
+			m_listID = listID;
+			m_listName = listName;
+		}
+
+		public void AddItem(ToDoItem item)
+		{
+
+		}
+
+		public void RemoveItem()
+		{
+
+		}
+
+		public List<ToDoItem> GetItems()
+		{
+			List<ToDoItem> itemList = new();
+			return itemList;
+		}
+	}
+}
