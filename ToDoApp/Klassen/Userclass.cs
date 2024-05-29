dbece084d2f68325d4fb2b54ba39ddaa451b3692
Sebastian Kuckluck <ghoma@outlook.de>
2024-05-29 10:08:30 +0200

    Klasse Userclass erstellt

---
+++ ToDoApp/Klassen/Userclass.cs
@@ -0,0 +1,33 @@
+﻿namespace ToDoApp.Klassen
+{
+	public class Userclass
+	{
+		#region Member
+		private int m_userID;
+		private string m_username;
+		private string m_password;
+		#endregion
+
+		#region Parameter
+		#endregion
+
+		public Userclass(int ID, string Name, string Password)
+		{
+			m_userID = ID;
+			m_username = Name;
+			m_password = Password;
+		}
+
+		#region Methoden
+		public void Login()
+		{
+
+		}
+
+		public void Logout()
+		{
+
+		}
+		#endregion
+	}
+}
