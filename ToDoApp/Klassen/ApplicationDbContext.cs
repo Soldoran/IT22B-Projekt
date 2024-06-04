f746fe41b3bf3be7eb8e5f7f046b2c94c6854bed
Sebastian <Ghoma@outlook.de>
2024-06-04 23:10:24 +0200

    Alpha-Version
    Anlegen und verschieben von ToDoItems
    Registrieren eines neuen Nutzers + schreiben in die (interne) Datenbank
    "Login" eines Nutzers, wenn dieser in der Datenbank registriert ist.

---
+++ ToDoApp/Klassen/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp.Shared;
+
+namespace ToDoApp.Klassen
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public DbSet<Userclass> Userclasses { get; set; }
+        public DbSet<ToDoList> ToDoLists { get; set; }
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DbConnectionString"));
+        }
+    }
+}
